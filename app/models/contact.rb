3579b0e909002eb0f0578e2cbb27cfa6b97b162a
Sadhvi Tripathi <sadhvi.tripathi23@gmail.com>
2021-06-16 14:11:56 +0530

    Added validations on contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,7 @@
 class Contact < ApplicationRecord
+  # Validations
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, format: { with: /\A\S+@\S+\z/, message: :email_format}
+  validates :message, presence: true
 end
