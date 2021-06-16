d693344be24cce22b3fc34eba1293714d7a3feef
Sadhvi Tripathi <sadhvi.tripathi23@gmail.com>
2021-06-16 18:42:37 +0530

    Code cleanup and optimization in controller and view files

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,23 +1,12 @@
 class ContactsController < ApplicationController
   protect_from_forgery
-  before_action :set_contact, only: %i[show]
 
-  # GET /contacts or /contacts.json
-  def index
-    @contacts = Contact.all
-  end
-
-  # GET /contacts/1 or /contacts/1.json
-  def show
-    @contact
-  end
-
-  # GET /contacts/new
+  # GET new form
   def new
     @contact = Contact.new
   end
 
-  # POST /contacts
+  #  Create contacts
   def create
     @contact = Contact.new(contact_params)
     respond_to do |format|
@@ -30,13 +19,9 @@ class ContactsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_contact
-      @contact = Contact.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def contact_params
-      params.permit('first_name', 'last_name', 'email', 'phone_number', 'message', 'locale')
-    end
+  # Only allow a list of trusted parameters through.
+  def contact_params
+    params.permit('first_name', 'last_name', 'email', 'phone_number', 'message', 'locale')
+  end
 end
---
+++ test/mailers/user_mailer_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class UserMailerTest < ActionMailer::TestCase
+  test 'new contact email' do
+
+    # Set up an email using the user details
+    email = UserMailer.with(email: 'info@example.com', contact_info: { locale: 'en', first_name: 'ABC',
+                                                                       last_name: 'XYZ', phone_number: '',
+                                                                       email: 'xyz@abc.com' }).notify_on_contact_creation.deliver_now
+
+    # Check the contents are correct
+    assert_equal 'New contact has been created, here are some details', email.subject
+    assert_match 'ABC', email.text_part.body.encoded
+    assert_match 'XYZ', email.text_part.body.encoded
+    assert_match 'xyz@abc.com', email.text_part.body.encoded
+  end
+end
